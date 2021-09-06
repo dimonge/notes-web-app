a113540c86b999a3b2fe2d0422389f597d9f0920
Peter Shodeinde <petershodeinde4@gmail.com>
2021-09-06 21:25:36 +0300

    implementing the note api

---
+++ src/create.js
@@ -0,0 +1,21 @@
+import * as uuid from "uuid";
+
+import dynamodb from "./utils/dynamodb";
+
+export const main = handler(async (event) => {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      userId: "123",
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
+
+  await dynamodb.put(params);
+  return params.Item;
+});
